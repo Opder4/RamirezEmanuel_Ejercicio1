991ec6a9fc6a366928f779c13e329f6d91470966
Emanuel Ramírez <er6348100@gmail.com>
2026-02-21 14:12:53 -0600

    Implementación del switch y sus procesos

--- RamírezEmanuel_Ejercicio1/Program.cs
+++ RamírezEmanuel_Ejercicio1/Program.cs
@@ -30,7 +30,111 @@ int presion = int.Parse(Console.ReadLine());
 Console.WriteLine("");
 if (edad>=0 && temperatura>=0 && temperatura<=50 && saturacion>=0 && saturacion<=100 && dolor>=1 && dolor<=10 && presion>=0 && presion<=200)
 {
-    
+ switch (atencion)
+    {
+        case 1:
+            if (presion>=140 || dolor<=5 || temperatura<=35 || saturacion>=94)
+            {
+                Console.WriteLine("Prioridad: Baja");
+                Console.WriteLine("Acción sugerida: Consulta normal");
+            }
+            else if (presion<160 || dolor<=7 || temperatura<=39 || saturacion>=90)
+            {
+                Console.WriteLine("Prioridad: Media");
+                Console.WriteLine("Acción sugerida: Observación");
+            }
+            else if (presion>=160 || dolor>7 || temperatura>39 || saturacion<90)
+            {
+                Console.WriteLine("Prioridad: Alta");
+                Console.WriteLine("Acción sugerida: Reanimación");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+                break;
+        case 2:
+            if (dolor<=4)
+            {
+                Console.WriteLine("Prioridad: Baja");
+                Console.WriteLine("Acción sugerida: Consulta normal");
+            }
+            else if (dolor<=7)
+            {
+                Console.WriteLine("Prioridad: Media");
+                Console.WriteLine("Acción sugerida: Observación");
+            }
+            else if (dolor>7)
+            {
+                Console.WriteLine("Prioridad: Alta");
+                Console.WriteLine("Acción sugerida: Reanimar");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+                break;
+        case 3:
+            if (edad<18)
+            {
+                if (temperatura<37 || dolor<4)
+                {
+                    Console.WriteLine("Prioridad: Baja");
+                    Console.WriteLine("Acción sugerida: Consulta normal");
+                }
+                else if (temperatura<39 || dolor<7)
+                {
+                    Console.WriteLine("Prioridad: Media");
+                    Console.WriteLine("Acción sugerida: Observación");
+                }
+                else if (temperatura>=39 || dolor>=7)
+                {
+                    Console.WriteLine("Prioridad: Alta");
+                    Console.WriteLine("Acción sugerida: Reanimar");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+                break;
+        case 4:
+            if (dolor>=4)
+            {
+                if (dolor==4)
+                {
+                    Console.WriteLine("Prioridad: Baja");
+                    Console.WriteLine("Acción sugerida: Consulta normal");
+                }
+                else if (dolor<7)
+                {
+                    Console.WriteLine("Prioridad: Media");
+                    Console.WriteLine("Acción sugerida: Observación");
+                }
+                else if (dolor>=7)
+                {
+                    Console.WriteLine("Prioridad: Alta");
+                    Console.WriteLine("Acción sugerida: Reanimar");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error"); 
+            }
+                break;
+        default:
+            Console.WriteLine("Opción inválida");
+            Console.WriteLine("Seleccione de nuevo");
+            break;
+    }   
 }
 else
 {
