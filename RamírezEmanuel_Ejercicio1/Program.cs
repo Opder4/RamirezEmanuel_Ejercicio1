638f56f0349348522eb60af2c7332044665cc4e7
Emanuel Ramírez <er6348100@gmail.com>
2026-02-21 13:20:05 -0600

    Validación de datos

--- RamírezEmanuel_Ejercicio1/Program.cs
+++ RamírezEmanuel_Ejercicio1/Program.cs
@@ -12,11 +12,11 @@ Console.WriteLine("Ingrese la edad del paciente: ");
 Console.WriteLine("");
 int edad=int.Parse(Console.ReadLine());
 Console.WriteLine("");
-Console.WriteLine("Ingrese la temperatura del paciente: ");
+Console.WriteLine("Ingrese la temperatura del paciente (°C): ");
 Console.WriteLine("");
 double temperatura = double.Parse(Console.ReadLine());
 Console.WriteLine("");
-Console.WriteLine("Ingrese la saturación de oxígeno del paciente: ");
+Console.WriteLine("Ingrese la saturación de oxígeno del paciente (%): ");
 Console.WriteLine("");
 double saturacion = double.Parse(Console.ReadLine());
 Console.WriteLine("");
@@ -24,7 +24,16 @@ Console.WriteLine("Ingrese el nivel de dolor del paciente (1-10): ");
 Console.WriteLine("");
 int dolor = int.Parse(Console.ReadLine());
 Console.WriteLine("");
-Console.WriteLine("Ingrese la presión sistólica del paciente: ");
+Console.WriteLine("Ingrese la presión sistólica del paciente (mmHg): ");
 Console.WriteLine("");
 int presion = int.Parse(Console.ReadLine());
 Console.WriteLine("");
+if (edad>=0 && temperatura>=0 && temperatura<=50 && saturacion>=0 && saturacion<=100 && dolor>=1 && dolor<=10 && presion>=0 && presion<=200)
+{
+    
+}
+else
+{
+    Console.WriteLine("Los datos son inválidos");
+    Console.WriteLine("Por favor ingrese valores que sean válidos");
+}
